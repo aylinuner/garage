5f809423be1cfb358394e08409ae942f77882e41
aylinuner <unerayliin@gmail.com>
2025-06-20 11:45:47 +0300

    next

--- driver/detail.js
+++ driver/detail.js
@@ -1,6 +1,7 @@
 const grg_ls = JSON.parse(localStorage.getItem('grg_ls'))
 var driver_model = {
     img: null,
+    img_byte_array: null,
     id: null,
     name: null,
     surname: null,
@@ -32,8 +33,8 @@ document.addEventListener('DOMContentLoaded', (e) => {
     if (grg_ls.signed_user.name) {
         document.getElementById("logged_user_name").textContent = grg_ls.signed_user.name;
     }
-    updateDriver()
-    deleteDriver()
+    setDriver()
+    // deleteDriver()
 });
 
 
@@ -45,10 +46,13 @@ function logout() {
     window.location.href = "/user.html"
 }
 
-function saveDriver() {
+async function saveDriver() {
+    const imgBytes = await readFileAsBytes();
+
     driver_model = {
         id: generateId(grg_ls.db.driver),
-        // img:document.getElementById('img').value.trim(),
+        img: document.getElementById('img').value,
+        img_byte_array: imgBytes,
         name: document.getElementById('name').value.trim(),
         surname: document.getElementById('surname').value.trim(),
         tc: document.getElementById('tc').value.trim(),
@@ -56,8 +60,9 @@ function saveDriver() {
         city: document.getElementById('city').value.trim(),
         state: document.getElementById('state').value.trim(),
         zip: document.getElementById('zip').value.trim(),
-        driver_file: document.getElementById("driver_file").value.trim()
+        driver_file: document.getElementById("driver_file").value
     }
+    console.log(driver_model)
     const exist_tc = grg_ls.db.driver.some(x => x.tc === driver_model.tc)
 
     if (exist_tc) {
@@ -76,16 +81,17 @@ function saveDriver() {
 function generateId(p) {
     return p.reduce((max, u) => Math.max(max, u.id || 0), 0) + 1;
 }
-function updateDriver() {
+function setDriver() {
+
     const id = getQueryParam("id");
     const grg_ls = JSON.parse(localStorage.getItem("grg_ls"));
     if (id) {
         document.getElementById("update_btn").classList.remove("d-none");
-
         const numericId = parseInt(id);
         const driver = grg_ls.db.driver.find(v => v.id === numericId);
         // Sayfa yüklendiğinde inputlara verileri yerleştir
         // document.getElementById("img").value=driver.img;
+        imgByteArrayToImgFile(driver);
         document.getElementById("name").value = driver.name;
         document.getElementById("surname").value = driver.surname;
         document.getElementById("tc").value = driver.tc;
@@ -93,11 +99,19 @@ function updateDriver() {
         document.getElementById("city").value = driver.city;
         document.getElementById("state").value = driver.state;
         document.getElementById("zip").value = driver.zip;
+        // document.getElementById("driver_img").value = driver.img
+        if(driver.img_byte_array && driver.img_byte_array.length >0){
+            const byteArray=new Uint8Array(driver.img_byte_array);
+            const blob=new Blob([byteArray], {type: "image/jpeg/webp"})
+            const imageUrl=URL.createObjectURL(blob);
+            document.getElementById("driver_img").src=imageUrl;
+        }
+
         // document.getElementById("driver_file").value = driver.driver_file;
 
         const updateBtn = document.getElementById("update_btn");
         updateBtn.classList.remove("d-none");
-        updateBtn.onclick = () => {
+        updateBtn.onclick = async () => {
             // driver.img=document.getElementById("img").value;
             driver.name = document.getElementById("name").value;
             driver.surname = document.getElementById("surname").value;
@@ -107,12 +121,15 @@ function updateDriver() {
             driver.state = document.getElementById("state").value;
             driver.zip = document.getElementById("zip").value;
             // driver.driver_file = document.getElementById("driver_file").value;
-
+            const imgInput = document.getElementById("img");
+            if (imgInput.files.length > 0) {
+                driver.img = imgInput.value;
+                driver.img_byte_array = await readFileAsBytes();
+            }
             localStorage.setItem("grg_ls", JSON.stringify(grg_ls));
             alert("Sürücü bilgisi başarıyla güncellendi!");
             window.location.href = "index.html";
         };
-
     }
 }
 function deleteDriver() {
@@ -122,7 +139,8 @@ function deleteDriver() {
     const driverIndex = grg_ls.db.driver.findIndex(d => d.id === numericId);
 
     const driver = grg_ls.db.driver[driverIndex];
-    // document.getElementById("img").value=driver.img;
+    document.getElementById("img").value = driver.img;
+    // dpocument.getElementById("img_byte_array").value=driver.img_byte_array;
     document.getElementById("name").value = driver.name;
     document.getElementById("surname").value = driver.surname;
     document.getElementById("tc").value = driver.tc;
@@ -145,4 +163,48 @@ function deleteDriver() {
 function getQueryParam(param) {
     const urlParams = new URLSearchParams(window.location.search);
     return urlParams.get(param);
+}
+function readFileAsBytes() {
+    debugger
+    return new Promise((resolve, reject) => {
+
+        const input = document.getElementById('img')
+        const file = input.files[0];
+        if (!file) {
+            alert("Lütfen bir dosya seçin")
+            return;
+        }
+        const reader = new FileReader();
+
+        reader.onload = function (event) {
+            const arrayBuffer = event.target.result;
+            const bytes = new Uint8Array(arrayBuffer);
+            console.log("byte Array", bytes);
+            resolve(bytes);
+        };
+        reader.onerror = function (error) {
+            reject(error);
+        }
+        reader.readAsArrayBuffer(file);
+    });
+}
+function imgByteArrayToImgFile(driver_model) {
+    if (!driver_model.img_byte_array || driver_model.img_byte_array.length === 0) {
+        console.warn("img_byte_array boş");
+        return null;
+    }
+
+    const byteArray = new Uint8Array(driver_model.img_byte_array);
+
+    const blob = new Blob([byteArray], { type: 'image/jpeg/webp' });
+
+    const imgElement = document.getElementById('driver_img')
+    if (imgElement) {
+        const imageUrl = URL.createObjectURL(blob);
+        imgElement.src = imageUrl;
+    } else {
+        console.warn("driver_img elementi bulunamadı")
+    }
+    const file = new File([blob], "driver_photo.jpg", { type: 'image/jpeg/webp' });
+    return file;
 }
